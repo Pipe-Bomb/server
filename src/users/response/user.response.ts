75a9732c5d93171947d73294eef5d838956eab9b
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2026-06-02 21:56:42 +1200

    Update user.response.ts

--- src/users/response/user.response.ts
+++ src/users/response/user.response.ts
@@ -10,7 +10,7 @@ export class UserResponse {
 	username: string;
 
 	@ApiProperty({
-		type: [PlaylistResponse],
+		type: () => [PlaylistResponse],
 		nullable: true,
 	})
 	playlists: PlaylistResponse[] | null;
