399860d45bedefbb9de536538ec5160e63e3b6e2
eyezah (Teenybook) <50851883+eyezahhhh@users.noreply.github.com>
2026-08-23 23:02:39 +1200

    Update search.controller.ts

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { SearchService } from "./search.service";
 import { ApiOkResponse, ApiOperation } from "@nestjs/swagger";
 import { SearchDto } from "./dto/search.dto";
@@ -9,7 +9,7 @@ export class SearchController {
 	constructor(private readonly searchService: SearchService) {}
 
 	@Post()
-	@HttpCode(200)
+	@HttpCode(HttpStatus.OK)
 	@ApiOperation({ operationId: "search" })
 	@ApiOkResponse({
 		type: SearchResultsResponse,
